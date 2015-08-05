b50353b7e5ac248d724324e97625e7668dcdc9ae
Emanuel <rhodesemanuel@yahoo.com>
2015-08-05 08:52:21 -0400

    Again again
    4th times a charm...still working on comments

---
+++ Week-5/redditclone4/app/models/topic.rb
@@ -0,0 +1,7 @@
+class Topic < ActiveRecord::Base
+  has_many :votes
+  belongs_to :users
+  has_many :comments
+
+
+end
---
+++ Week-5/redditclone4/app/models/vote.rb
@@ -0,0 +1,6 @@
+class Vote < ActiveRecord::Base
+  belongs_to :topic
+  belongs_to :users
+
+  
+end
